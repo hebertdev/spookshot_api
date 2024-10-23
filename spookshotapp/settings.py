6ae05ef8e66727f841375d88af77ab7e12b10b08
hebertdev <hebert.villafuerte.c@hotmail.com>
2024-10-22 22:23:02 -0500

    fixing

--- spookshotapp/settings.py
+++ spookshotapp/settings.py
@@ -160,4 +160,9 @@ REST_FRAMEWORK = {
 
 
 import cloudinary
-cloudinary.config(secure=True)
+cloudinary.config(
+    cloud_name=config('CLOUDINARY_CLOUD_NAME'),
+    api_key=config('CLOUDINARY_API_KEY'),
+    api_secret=config('CLOUDINARY_API_SECRET'),
+    secure=True
+)
